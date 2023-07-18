dd5fe792799ca14c05ad6311681d29eba63b68a8
likianta <likianta@foxmail.com>
2023-07-18 19:05:39 +0800

    Create `lkfmt`

---
+++ projects/lkfmt/lkfmt/lk_flavor.py
@@ -0,0 +1,39 @@
+"""
+likianta flavored code formatting style.
+    - keep indents on empty lines
+    - align dict key-value pairs by colon
+    - align variable assignments by equal sign
+"""
+import re
+import typing as t
+
+import lk_logger
+from lk_logger.scanner import get_all_blocks
+
+
+def keep_indents(code: str) -> str:
+    _leading_spaces = re.compile(r'^ *')
+    
+    def walk() -> t.Iterator[str]:
+        lines0 = tuple(code.splitlines())
+        lines1 = lines0[1:] + ('',)
+        
+        for curr_line, next_line in zip(lines0, lines1):
+            if not curr_line:
+                if next_line:
+                    yield '{}{}'.format(
+                        _leading_spaces.match(next_line).group(0),
+                        curr_line
+                    )
+                    continue
+            yield curr_line
+    
+    return '\n'.join(walk())
+
+
+def align_dict_items(code: str) -> str:
+    pass
+
+
+def align_vars_assignments(code: str) -> str:
+    pass
