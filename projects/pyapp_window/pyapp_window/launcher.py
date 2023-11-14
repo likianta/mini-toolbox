58ba363f77f633be6a4dcabbb6727945cdd8347d
Likianta <likianta@foxmail.com>
2023-11-14 11:48:25 +0800

    Extend copilot_backend type

--- projects/pyapp_window/pyapp_window/launcher.py
+++ projects/pyapp_window/pyapp_window/launcher.py
@@ -1,5 +1,7 @@
+import typing as t
 from multiprocessing import Process
 from subprocess import Popen
+
 from time import sleep
 
 from .native_window import open_native_window
@@ -8,17 +10,22 @@ from .native_window import open_native_window
 def launch(
     title: str,
     url: str,
-    copilot_backend: Popen = None,
+    copilot_backend: t.Union[Popen, t.Callable] = None,
     wait_url_ready: bool = False,
     **kwargs
 ) -> None:
     if copilot_backend:
-        proc_back = copilot_backend
+        if isinstance(copilot_backend, Popen):
+            proc_back = copilot_backend
+        else:
+            proc_back = Process(target=copilot_backend, daemon=True)
+            proc_back.start()
+        
         proc_front = Process(
             target=open_native_window,
             kwargs={
-                'title': title,
-                'url'  : url,
+                'title'    : title,
+                'url'      : url,
                 'check_url': wait_url_ready,
                 **kwargs
             },
@@ -30,9 +37,14 @@ def launch(
             if not proc_front.is_alive():
                 proc_back.terminate()
                 break
-            if proc_back.poll() is not None:
-                proc_front.terminate()
-                break
+            if isinstance(proc_back, Popen):
+                if proc_back.poll() is not None:
+                    proc_front.terminate()
+                    break
+            else:
+                if not proc_back.is_alive():
+                    proc_front.terminate()
+                    break
             sleep(1)
     else:
         open_native_window(
