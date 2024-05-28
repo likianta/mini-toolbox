51c66f51a4234156c1477b6b2ccb9aae6b1c412e
likianta <likianta@foxmail.com>
2024-05-28 17:25:12 +0800

    [pyapp_window] remove project from this repo

--- projects/pyapp_window/pyapp_window/__init__.py
+++
@@ -1,6 +0,0 @@
-from .launcher import launch
-from .webview_window import open_native_window
-from .webview_window.util import get_center_pos
-from .webview_window.util import get_screen_size
-
-__version__ = '0.5.0'
