042f915fbff5f4ca19ec8a2e94022d262cec0d29
Likianta <likianta@foxmail.com>
2025-06-05 17:51:18 +0800

    update poetry extensions

---
+++ projects/poetry_extensions/find_unused_dependencies.py
@@ -0,0 +1,98 @@
+"""
+use ast to parse the source code and find all imports.
+if imports in pyproject.toml are defined but not used across the source code, -
+print them out.
+"""
+import ast
+
+import lk_logger
+from argsense import cli
+from lk_utils import fs
+
+lk_logger.update(show_varnames=True)
+
+
+@cli.cmd()
+def main(
+    source_dir: str,
+    include_dev_group: bool = False,
+    ignored: str = None,
+) -> None:
+    """
+    params:
+        include_dev_group (-d):
+        ignored (-i):
+        
+    required:
+        <user_project>
+        |- <source>         # <- `source_dir`
+        |- pyproject.toml
+    """
+    project_dir = fs.parent(source_dir)
+    assert fs.exists(f'{project_dir}/pyproject.toml')
+    
+    imported_packages = set()  # {pkg_name, ...}
+    # {`aaa_bbb`: `ccc-ddd`, ...}
+    known_aliases = {
+        'code_editor': 'streamlit-code-editor',
+        'google'     : 'google-cloud-translate',
+        'hid'        : 'hidapi',
+        'pil'        : 'pillow',
+        'serial'     : 'pyserial',
+        'yaml'       : 'pyyaml',
+        'zmq'        : 'pyzmq',
+    }
+    for f in fs.findall_files(source_dir, ".py"):
+        code = fs.load(f.path)
+        tree = ast.parse(code)
+        for node in ast.walk(tree):
+            if isinstance(node, ast.Import):
+                for alias in node.names:
+                    top_name = alias.name.split(".")[0].lower()
+                    pkg_name = known_aliases.get(
+                        top_name, top_name.replace("_", "-")
+                    )
+                    imported_packages.add(pkg_name)
+            elif isinstance(node, ast.ImportFrom):
+                if node.module:
+                    top_name = node.module.split(".")[0].lower()
+                    pkg_name = known_aliases.get(
+                        top_name, top_name.replace("_", "-")
+                    )
+                    imported_packages.add(pkg_name)
+    print(len(imported_packages))
+    
+    defined_packages = set()
+    conf = fs.load(f"{project_dir}/pyproject.toml")
+    for x in conf["tool"]["poetry"]["dependencies"]:
+        if x == "python":
+            continue
+        defined_packages.add(x)
+    if "group" in conf["tool"]["poetry"]:
+        for k, x in conf["tool"]["poetry"]["group"].items():
+            if k == 'dev' and not include_dev_group:
+                continue
+            for y in x["dependencies"]:
+                defined_packages.add(y)
+    print(len(defined_packages))
+    
+    ignored = set() if ignored is None else set(ignored.split(','))
+    if 'poetry-extensions' in conf['tool']:
+        ignored.update(
+            conf['tool']['poetry-extensions']['ignored-unused-packages']
+        )
+    
+    flag = 0
+    for x in sorted(defined_packages):
+        if x not in imported_packages:
+            if x not in ignored:
+                flag = 1
+                print(x, ":iv6")
+    if not flag:
+        print(':v4', 'satisfying all packages.')
+
+
+if __name__ == "__main__":
+    # pox projects/poetry_extensions/find_unused_dependencies.py <src_dir>
+    # pox projects/poetry_extensions/find_unused_dependencies.py <src_dir> :true
+    cli.run(main)
---
+++ projects/poetry_extensions/init_template.py
@@ -0,0 +1,81 @@
+import os
+import re
+
+import lk_logger
+from argsense import cli
+from lk_utils import dedent
+from lk_utils import fs
+
+lk_logger.setup(quiet=True, show_varnames=True)
+
+template = dedent(
+    """
+    [project]
+    name = "<package_name>"
+    version = "0.1.0"
+    description = ""
+    # readme = "README.md"
+    authors = [{ name = "Likianta", email = "likianta@foxmail.com" }]
+    packages = [{ include = "<package_name_snakecase>" }]
+    requires-python = ">=<pyversion>"
+    dynamic = ["dependencies"]
+    # license = "MIT"
+    
+    # [project.urls]
+    # homepage = ""
+    
+    [tool.poetry.dependencies]
+    python = "^<pyversion>"
+    
+    [[tool.poetry.source]]
+    name = "tsinghua"
+    url = "https://pypi.tuna.tsinghua.edu.cn/simple/"
+    priority = "primary"
+    
+    [[tool.poetry.source]]
+    name = "likianta-host"
+    url = "http://47.102.108.149:2131/"
+    priority = "supplemental"
+
+    [build-system]
+    requires = ["poetry-core"]
+    build-backend = "poetry.core.masonry.api"
+    """
+)
+
+
+@cli
+def main(
+    target_dir: str,
+    package_name: str = None,
+    pyversion: str = '3.12',
+) -> None:
+    """
+    params:
+        pyversion (-v):
+    """
+    if target_dir is None:
+        target_dir = os.getcwd()
+    if package_name is None:
+        package_name = (
+            fs.dirname(target_dir)
+            .lower()
+            .replace('_', '-')
+            .replace(' ', '-')
+        )
+    assert re.match(r'[-a-z]+', package_name)
+    print(package_name, ':v2')
+    
+    output = (
+        template
+        .replace('<package_name>', package_name)
+        .replace('<package_name_snakecase>', package_name.replace('-', '_'))
+        .replace('<pyversion>', pyversion)
+    )
+    fs.dump(output, target_dir + '/pyproject.toml', 'plain')
+
+
+if __name__ == '__main__':
+    # pox projects/poetry_extensions/init_template.py <dir>
+    # pox projects/poetry_extensions/init_template.py <dir> -v 3.8
+    cli.run(main)
