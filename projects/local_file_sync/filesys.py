bf49c5e79e38faddf61621ee88036d6baba95dff
Likianta <likianta@foxmail.com>
2025-06-21 14:10:10 +0800

    create `local_file_sync`

---
+++ projects/local_file_sync/filesys.py
@@ -0,0 +1,320 @@
+import ftplib
+import hashlib
+import io
+import json
+import re
+import typing as t
+from datetime import datetime
+from lk_utils import fs
+from time import time
+
+
+class T:
+    Path = str
+    Time = int
+    SnapshotData = t.Dict[Path, Time]  # {relpath: modified_time, ...}
+    SnapshotItem = t.TypedDict('SnapshotItem', {
+        'version': str,  # `<hash>-<time>`
+        'data'   : SnapshotData,
+    })
+    SnapshotFull = t.TypedDict('SnapshotFull', {
+        'base'   : SnapshotItem,
+        'current': SnapshotItem,
+    })
+
+
+class BaseFileSystem:
+    def __init__(self, root: T.Path) -> None:
+        self.root = root
+        self.snapshot_file = f'{root}/snapshot.json'
+    
+    def load_snapshot(self) -> T.SnapshotFull:
+        return self.load(self.snapshot_file)
+    
+    def update_snapshot(self, data: T.SnapshotData) -> None:
+        full: T.SnapshotFull
+        if self.exist(self.snapshot_file):
+            full = self.load_snapshot()
+            full['current']['version'] = '{}-{}'.format(
+                self._hash_snapshot(data), int(time())
+            )
+            full['current']['data'] = data
+            self.dump(full, self.snapshot_file)
+        else:
+            self.rebuild_snapshot(data)
+        
+    def rebuild_snapshot(self, data: T.SnapshotData) -> None:
+        full = {
+            'base'   : (x := {
+                'version': '{}-{}'.format(
+                    self._hash_snapshot(data), int(time())
+                ),
+                'data'   : data,
+            }),
+            'current': x
+        }
+        self.dump(full, self.snapshot_file)
+    
+    # -------------------------------------------------------------------------
+    
+    def dump(self, data: t.Any, file: T.Path) -> None:
+        raise NotImplementedError
+    
+    def exist(self, path: T.Path) -> bool:
+        raise NotImplementedError
+    
+    def findall_files(self) -> t.Iterable[t.Tuple[T.Path, int]]:
+        raise NotImplementedError
+    
+    def load(self, file: T.Path) -> t.Any:
+        raise NotImplementedError
+    
+    def make_dirs(self, dirpath: T.Path) -> None:
+        raise NotImplementedError
+    
+    def remove(self, file: T.Path) -> None:
+        raise NotImplementedError
+    
+    @staticmethod
+    def _hash_snapshot(data: T.SnapshotData) -> str:
+        return hashlib.md5(
+            json.dumps(data, sort_keys=True).encode()
+            #                ~~~~~~~~~~~~~~ recursively sort.
+            #   the order of keys affects hash result. since -
+            #   `FtpFileSystem.findall_files` has a random order, we need to -
+            #   sort them.
+        ).hexdigest()
+
+
+class LocalFileSystem(BaseFileSystem):
+    def __init__(self, root: T.Path) -> None:
+        super().__init__(fs.abspath(root))
+    
+    def dump(self, data: t.Any, file: T.Path, binary: bool = False) -> None:
+        fs.dump(data, file, type='binary' if binary else 'auto')
+        
+    def exist(self, path: T.Path) -> bool:
+        return fs.exist(path)
+    
+    def findall_files(self) -> t.Iterator[t.Tuple[T.Path, int]]:
+        for f in fs.findall_files(self.root):
+            yield f.path, fs.filetime(f.path)
+    
+    def load(self, file: T.Path, binary: bool = False) -> t.Any:
+        return fs.load(file, type='binary' if binary else 'auto')
+    
+    def make_dirs(self, dirpath: T.Path) -> None:
+        assert dirpath.startswith(self.root)
+        if not fs.exist(dirpath):
+            fs.make_dirs(dirpath)
+    
+    def remove(self, file: T.Path) -> None:
+        fs.remove_file(file)
+
+
+class FtpFileSystem(BaseFileSystem):
+    def __init__(self, url: str) -> None:
+        a, b, c, d = url.split('/', 3)
+        assert a == 'ftp:' and b == '' and d.startswith('Likianta/')
+        e, f = c.rsplit(':', 1)
+        host, port, root = e, int(f), f'/{d}'
+        
+        super().__init__(root)
+        
+        self._ftp = ftplib.FTP()
+        self._ftp.connect(host, port)
+        self._ftp.login()
+        self._ftp.cwd(root)
+    
+    def load_snapshot(self) -> T.SnapshotFull:
+        data_bytes = self.load(self.snapshot_file)
+        return json.loads(data_bytes)
+    
+    def download_file(self, file_i: T.Path, file_o: T.Path) -> None:
+        pass  # TODO
+    
+    def dump(self, data: t.Any, file: T.Path) -> None:
+        # file = self._normpath(file)
+        
+        if isinstance(data, bytes):
+            data_bytes = data
+        elif isinstance(data, dict):
+            text = json.dumps(data, indent=2, ensure_ascii=False)
+            data_bytes = text.encode('utf-8')
+        else:
+            raise NotImplementedError
+        
+        with io.BytesIO(data_bytes) as f:
+            self._ftp.storbinary(f'STOR {file}', f)
+            #   note: we don't need to quote `file` even it has -
+            #   whitespaces. i.e. 'STOR 01 02.txt' is legal.
+            #   besides, 'STOR "01 02.txt"' will report an error.
+    
+    def exist(self, path: T.Path) -> bool:
+        # path = self._normpath(path)
+        a, b = path.rsplit('/', 1)
+        if b[0] == '.':
+            for f, _ in self._find_hidden_paths(a):
+                if f.endswith('/' + b):
+                    return True
+        else:
+            for name in self._ftp.nlst(a):
+                if name == b:
+                    return True
+        return False
+    
+    def findall_files(self) -> t.Iterator[t.Tuple[T.Path, int]]:
+        def recursive_find(dir: T.Path) -> t.Iterator[
+            t.Tuple[str, t.Optional[dict]]
+        ]:
+            subdirs = []
+            # FIXME: ftp.msld doesn't show hidden files.
+            for name, info in self._ftp.mlsd(dir):
+                if info['type'] == 'file':
+                    yield f'{dir}/{name}', info
+                elif info['type'] == 'dir':
+                    subdirs.append(f'{dir}/{name}')
+                    # yield from self.findall_files(f'{dir}/{name}')
+                else:
+                    raise Exception(dir, name, info)
+            for path, type in self._find_hidden_paths(dir):
+                if type == 'dir':
+                    subdirs.append(path)
+                else:
+                    yield path, None
+            for subdir in subdirs:
+                yield from recursive_find(subdir)
+        
+        # https://stackoverflow.com/a/62334092/9695911
+        # https://stackoverflow.com/a/64480694/9695911
+        now = datetime.now()
+        current_timezone = now.astimezone(None).tzinfo
+        # print(current_timezone, ':v')
+        #   e.g. datetime.timezone(
+        #       datetime.timedelta(seconds=28800), 'China Standard Time'
+        #   )
+        _time_delta = current_timezone.utcoffset(now)
+        #   e.g. datetime.timedelta(seconds=28800)
+        # time_delta = current_timezone.utcoffset(now).seconds  # 28800
+        
+        _temp_file = fs.xpath('_temp/temp_hidden_file')
+        
+        def get_modify_time_of_hidden_file(file: T.Path) -> int:
+            # data = self.load(file)
+            # fs.dump(data, _temp_file, 'binary')
+            # return fs.filetime(_temp_file)
+            
+            file_i = file
+            a, b = file.rsplit('/', 1)
+            file_o = f'{a}/__{b}'
+            # print(':v', '{} -> {}'.format(file_i, file_o))
+            self._ftp.rename(file_i, file_o)
+            for name, info in self._ftp.mlsd(fs.parent(file)):
+                if name == f'__{b}':
+                    self._ftp.rename(file_o, file_i)
+                    return time_str_2_int(info['modify'])
+            else:
+                self._ftp.rename(file_o, file_i)
+                raise Exception(file)
+            
+        def time_str_2_int(mtime: str) -> int:
+            """
+            params:
+                mtime: e.g. '20250619064438.504'
+                    be noted this value is UTC0 time. we need to convert it to -
+                    our timezone.
+            """
+            dt = datetime(
+                *map(int, (
+                    mtime[0:4],
+                    mtime[4:6],
+                    mtime[6:8],
+                    mtime[8:10],
+                    mtime[10:12],
+                    mtime[12:14],
+                ))
+            )
+            dt += _time_delta
+            return int(dt.timestamp())
+        
+        hidden_files = []
+        for file, info in recursive_find(self.root):
+            if info is None:
+                hidden_files.append(file)
+            else:
+                yield file, time_str_2_int(info['modify'])
+        for file in hidden_files:
+            print(file, ':v6i')
+            yield file, get_modify_time_of_hidden_file(file)
+    
+    def load(self, file: T.Path) -> bytes:
+        with io.BytesIO() as f:
+            self._ftp.retrbinary(f'RETR {file}', f.write)
+            f.seek(0)
+            return f.read()
+    
+    def make_dirs(self, dirpath: T.Path) -> None:
+        assert dirpath.startswith(self.root)
+        if not self.exist(dirpath):
+            self._ftp.mkd(dirpath)
+    
+    def remove(self, file: T.Path) -> None:
+        self._ftp.delete(file)
+    
+    def upload_file(self, file_i: T.Path, file_o: T.Path) -> None:
+        # this method similar to `self.dump`, but keeps origin file's modify -
+        # time for target.
+        with open(file_i, 'rb') as f:
+            self._ftp.storbinary(f'STOR {file_o}', f)
+        self._ftp.sendcmd('MFMT {} {}'.format(
+            self._time_int_2_str(fs.filetime(file_i)), file_o
+        ))
+    
+    # noinspection PyTypeChecker
+    def _find_hidden_paths(self, dir: T.Path) -> t.Iterator[
+        t.Tuple[T.Path, t.Literal['dir', 'file']]
+    ]:
+        ls: t.List[str] = []
+        self._ftp.retrlines('LIST -a {}'.format(dir), ls.append)
+        pattern = re.compile(
+            r'([-d])rwx?-+ +'
+            r'0 user group +'
+            r'\d+ '  # size
+            r'\w+ +\d+ +(?:\d\d:\d\d|\d{4}) '  # time
+            r'(.+)'  # name
+        )
+        for line in ls:
+            assert (m := pattern.match(line)), line
+            a, b = m.groups()
+            if b[0] == '.':
+                yield f'{dir}/{b}', 'dir' if a == 'd' else 'file'
+    
+    # def _normpath(self, path: T.Path) -> T.Path:
+    #     if path == '.':
+    #         return self.root
+    #     elif path.startswith(('./', '../')):
+    #         return fs.normpath('{}/{}'.format(self.root, path))
+    #     else:
+    #         return path
+    
+    @staticmethod
+    def _time_int_2_str(mtime: int) -> str:
+        dt = datetime.fromtimestamp(mtime)
+        return dt.strftime('%Y%m%d%H%M%S')
+    
+    @staticmethod
+    def _time_str_2_int(mtime: str) -> int:
+        """
+        mtime: e.g. '20250619064438.504'
+        """
+        dt = datetime(
+            *map(int, (
+                mtime[0:4],
+                mtime[4:6],
+                mtime[6:8],
+                mtime[8:10],
+                mtime[10:12],
+                mtime[12:14],
+            ))
+        )
+        return int(dt.timestamp())
---
+++ projects/local_file_sync/main.py
@@ -0,0 +1,388 @@
+if __name__ == '__main__':
+    __package__ = 'projects.local_file_sync'
+
+import typing as t
+from argsense import cli
+from collections import defaultdict
+from lk_utils import fs as _fs
+from lk_utils import timestamp
+from .filesys import FtpFileSystem
+from .filesys import LocalFileSystem
+from .filesys import T as T0
+
+
+class T(T0):
+    Key = str  # a relpath
+    Movement = t.Literal['+>', '=>', '->', '<+', '<=', '<-']
+    #   +>  add to right
+    #   =>  overwrite to right
+    #   ->  delete to right
+    #   <+  add to left
+    #   <=  overwrite to left
+    #   <-  delete to left
+    #   ==  no change
+    ComposedAction = t.Tuple[Key, Movement, T0.Time]
+
+
+@cli
+def create_snapshot(root: str, rebuild: bool = False) -> None:
+    """
+    note: ftp work takes more time, depends on network traffic.
+    params:
+        rebuild (-r):
+    """
+    if root.startswith('ftp://'):
+        fs = FtpFileSystem(root)
+    else:
+        fs = LocalFileSystem(root)
+    root = fs.root  # a normalized path.
+    
+    data = {}
+    for f, t in fs.findall_files():
+        if f == f'{root}/snapshot.json':
+            continue
+        print(':i', _fs.relpath(f, root))
+        data[f.removeprefix(root + '/')] = t
+    
+    if rebuild:
+        fs.rebuild_snapshot(data)
+    else:
+        fs.update_snapshot(data)
+    
+    # TEST
+    if isinstance(fs, FtpFileSystem):
+        data_i = fs.load(fs.snapshot_file)
+        _fs.dump(
+            data_i,
+            r'C:\Likianta\temp\2025-06\snapshot-test.json',
+            'binary'
+        )
+    print(':t', 'done')
+
+
+@cli
+def sync_documents(root_a: str, root_b: str, dry_run: bool = False) -> None:
+    """
+    params:
+        root_a: root path on the left side. usually "master".
+        root_b: root path on the right side. usually "slave".
+        dry_run (-d):
+    """
+    assert not root_a.startswith('ftp://') and root_b.startswith('ftp://')
+    
+    fs_a = LocalFileSystem(root_a)
+    fs_b = FtpFileSystem(root_b)
+    
+    snap_a = fs_a.load_snapshot()
+    snap_b = fs_b.load_snapshot()
+    
+    # decide snap_data_base
+    snap_base_hash_a0, snap_base_time_a0 = snap_a['base']['version'].split('-')
+    snap_base_hash_b0, snap_base_time_b0 = snap_b['base']['version'].split('-')
+    if snap_base_hash_a0 == snap_base_hash_b0:
+        snap_data_base = snap_a['base']['data']
+    else:
+        # note: we prefer assuming snap_b to be the old one.
+        if snap_base_time_b0 <= snap_base_time_a0:
+            print('use snap_b0 as base')  # use the "old" one.
+            snap_data_base = snap_b['base']['data']
+        else:
+            print('use snap_a0 as base')
+            snap_data_base = snap_a['base']['data']
+    
+    snap_data_a = snap_a['current']['data']
+    snap_data_b = snap_b['current']['data']
+    
+    # noinspection PyTypeChecker
+    def compare_new_to_old(
+        snap_new: T.SnapshotData,
+        snap_old: T.SnapshotData,
+    ) -> t.Iterator[T.ComposedAction]:
+        """
+        note: the yieled movement can only be the following:
+            '+>', '=>', '->'.
+        """
+        for k, time_new in snap_new.items():
+            if k in snap_old:
+                time_old = snap_old[k]
+                # assert time_new >= time_old, k
+                # if time_new > time_old:
+                #     yield k, '=>', time_new
+                if time_new > time_old:
+                    yield k, '=>', time_new
+                elif time_new < time_old:
+                    print(':v6', k, time_new, time_old)
+                    # yield k, '<=?', time_old
+            else:
+                yield k, '+>', time_new
+        for k, time_old in snap_old.items():
+            if k not in snap_new:
+                yield k, '->', time_old
+    
+    # noinspection PyTypeChecker
+    def compare_changelists(
+        changes_a: t.Dict[T.Key, t.Tuple[T.Movement, T.Time]],
+        changes_b: t.Dict[T.Key, t.Tuple[T.Movement, T.Time]],
+    ) -> t.Iterator[T.ComposedAction]:
+        for k, (ma, ta) in changes_a.items():
+            if k in changes_b:
+                mb, tb = changes_b[k]
+                if ma == '+>' or ma == '=>':
+                    if mb == '+>' or mb == '=>':
+                        if ta >= tb:
+                            # b created/updated -> a created/updated
+                            yield k, '=>?', ta
+                        else:  # ta < tb
+                            # a created/updated -> b created/updated
+                            yield k, '<=?', tb
+                    else:
+                        # 1. ta > tb:
+                        #   b created -> b deleted -> a created/updated
+                        # 2. ta < tb:
+                        #   a created/updated -> b created -> b deleted
+                        # 3. ta < tb:
+                        #   b created -> a created/updated -> b deleted
+                        # 4. ta == tb:
+                        #   a b created/updated at same time -> b deleted
+                        yield k, '+>', ta
+                else:  # ma == '->'
+                    if mb == '+>' or mb == '=>':
+                        yield k, '<+', tb
+            else:
+                yield k, ma, ta
+        for k, (mb, tb) in changes_b.items():
+            if k not in changes_a:
+                if mb == '+>':
+                    yield k, '<+', tb
+                elif mb == '=>':
+                    yield k, '<=', tb
+                else:  # mb == '->'
+                    yield k, '<-', tb
+    
+    # PERF: fast compare by version of current against base.
+    changes_a = {
+        k: (m, t_) for k, m, t_ in
+        compare_new_to_old(snap_data_a, snap_data_base)
+    }
+    changes_b = {
+        k: (m, t_) for k, m, t_ in
+        compare_new_to_old(snap_data_b, snap_data_base)
+    }
+    final_changes = compare_changelists(changes_a, changes_b)
+    
+    # -------------------------------------------------------------------------
+    
+    if dry_run:
+        i = 0
+        table = [('index', 'left', 'action', 'right')]
+        action_count = defaultdict(int)
+        for k, m, _ in final_changes:
+            i += 1
+            colored_key = '[{}]{}[/]'.format(
+                'yellow' if '?' in m else
+                'green' if '+' in m else
+                'blue' if '=' in m else
+                'red',
+                k
+            )
+            # table.append((
+            #     str(i),
+            #     colored_key if m.startswith(('+>', '=>', '<-')) else
+            #     '' if m.startswith(('->', '<+')) else
+            #     '...',
+            #     m.rstrip('?'),
+            #     '' if m.startswith(('+>', '<-')) else
+            #     '...' if m.startswith(('=>',)) else
+            #     colored_key,
+            # ))
+            m = m.rstrip('?')
+            # noinspection PyTypeChecker
+            table.append((
+                str(i),
+                *(
+                    (colored_key, '+>', '') if m == '+>' else
+                    (colored_key, '=>', '...') if m == '=>' else
+                    ('', '->', colored_key) if m == '->' else
+                    ('', '<+', colored_key) if m == '<+' else
+                    ('...', '<=', colored_key) if m == '<=' else
+                    (colored_key, '<-', '')  # m == '<-'
+                )
+            ))
+            action_count[m.rstrip('?')] += 1
+        if len(table) > 1:
+            print(table, ':r2')
+            print(action_count, ':r2')
+        else:
+            print('no change', ':v3')
+        return
+    
+    def apply_changes(changes: t.Iterator[T.ComposedAction]) -> T.SnapshotData:
+        existed_dirs = set()
+        for f in snap_data_base:
+            d = ''
+            for x in f.split('/')[:-1]:
+                d += x + '/'
+                existed_dirs.add(d)
+        
+        # snap_new = snap_data_base.copy()
+        snap_new: T.SnapshotData = snap_data_base
+        
+        for k, m, t in changes:
+            # resolve conflict
+            if m.endswith('?'):  # '=>?', '<=?'
+                assert m in ('=>?', '<=?')
+                if m == '=>?':
+                    _backup_conflict_file_b('{}/{}'.format(fs_b.root, k))
+                else:
+                    _backup_conflict_file_a('{}/{}'.format(fs_a.root, k))
+                m = m[:-1]
+            # assert '?' not in m
+            
+            colored_key = '[{}]{}[/]'.format(
+                'green' if '+' in m else
+                'blue' if '=' in m else
+                'red',
+                k
+            )
+            # noinspection PyStringFormat
+            print(':ir', '{} {} {}'.format(
+                *(
+                    (colored_key, '+>', '') if m == '+>' else
+                    (colored_key, '=>', '...') if m == '=>' else
+                    ('', '->', colored_key) if m == '->' else
+                    ('', '<+', colored_key) if m == '<+' else
+                    ('...', '<=', colored_key) if m == '<=' else
+                    (colored_key, '<-', '')  # m == '<-'
+                )
+            ))
+            
+            # TODO: how to remove empty dirs which have no files inside?
+            # FIXME: how to sync modify time in `_update_file_a2b/b2a()`?
+            if m in ('+>', '=>'):
+                _make_dirs_b('{}/{}'.format(fs_b.root, k))
+                _update_file_a2b(k)
+                snap_new[k] = t
+            elif m == '->':
+                _delete_file_b('{}/{}'.format(fs_b.root, k))
+                snap_new.pop(k)
+            elif m in ('<+', '<='):
+                _make_dirs_a('{}/{}'.format(fs_a.root, k))
+                _update_file_b2a(k)
+                snap_new[k] = t
+            elif m == '<-':
+                _delete_file_a('{}/{}'.format(fs_a.root, k))
+                snap_new.pop(k)
+            else:
+                raise Exception(k, m, t)
+        
+        return snap_new
+    
+    _created_dirs_a = set()
+    for p in snap_data_a:
+        d = fs_a.root
+        for x in p.split('/')[:-1]:
+            d += '/' + x
+            _created_dirs_a.add(d)
+    _created_dirs_b = set()
+    for p in snap_data_b:
+        d = fs_b.root
+        for x in p.split('/')[:-1]:
+            d += '/' + x
+            _created_dirs_b.add(d)
+    
+    def _make_dirs_a(filepath: str) -> None:
+        i = filepath.rfind('/')
+        dirpath = filepath[:i]
+        if dirpath not in _created_dirs_a:
+            fs_a.make_dirs(dirpath)
+            _created_dirs_a.add(dirpath)
+    
+    def _make_dirs_b(filepath: str) -> None:
+        i = filepath.rfind('/')
+        dirpath = filepath[:i]
+        if dirpath not in _created_dirs_b:
+            fs_b.make_dirs(dirpath)
+            _created_dirs_b.add(dirpath)
+    
+    _conflicts_dir = '{}/{}'.format(
+        _fs.xpath('_conflicts'), x := timestamp('ymd_hns'))
+    _deleted_dir = '{}/{}'.format(_fs.xpath('_deleted'), x)
+    _fs.make_dir(_conflicts_dir)
+    _fs.make_dir(_deleted_dir)
+    
+    def _backup_conflict_file_a(file: T.Path) -> None:
+        file_i = file
+        m, n, o = file.rsplit('/', 2)
+        file_o = '{}/{}.a.{}'.format(_conflicts_dir, n, o)
+        _fs.copy_file(file_i, file_o)
+        
+    def _backup_conflict_file_b(file: T.Path) -> None:
+        file_i = file
+        m, n, o = file.rsplit('/', 2)
+        file_o = '{}/{}.b.{}'.format(_deleted_dir, n, o)
+        data_i = fs_b.load(file_i)
+        _fs.dump(data_i, file_o, 'binary')
+    
+    def _delete_file_a(file: T.Path) -> None:
+        file_i = file
+        m, n, o = file.rsplit('/', 2)
+        file_o = '{}/{}.a.{}'.format(_deleted_dir, n, o)
+        _fs.move(file_i, file_o)
+        # fs_a.remove(file)
+    
+    def _delete_file_b(file: T.Path) -> None:
+        file_i = file
+        m, n, o = file.rsplit('/', 2)
+        file_o = '{}/{}.b.{}'.format(_deleted_dir, n, o)
+        data_i = fs_b.load(file_i)
+        _fs.dump(data_i, file_o, 'binary')
+        fs_b.remove(file_i)
+    
+    def _update_file_a2b(relpath: T.Path) -> None:
+        file_i = '{}/{}'.format(fs_a.root, relpath)
+        file_o = '{}/{}'.format(fs_b.root, relpath)
+        data_i = fs_a.load(file_i, binary=True)
+        fs_b.dump(data_i, file_o)
+        
+    def _update_file_b2a(relpath: T.Path) -> None:
+        file_i = '{}/{}'.format(fs_b.root, relpath)
+        file_o = '{}/{}'.format(fs_a.root, relpath)
+        data_i = fs_b.load(file_i)
+        fs_a.dump(data_i, file_o, binary=True)
+    
+    snap_new = apply_changes(final_changes)
+    
+    if _fs.empty(_conflicts_dir):
+        _fs.remove_tree(_conflicts_dir)
+    else:
+        print('found {} conflicts, see in {}'.format(
+            len(_fs.find_file_names(_conflicts_dir)),
+            _fs.relpath(_conflicts_dir, _fs.xpath('..'))
+        ), ':v6')
+    if _fs.empty(_deleted_dir):
+        # _fs.remove_tree(_deleted_dir)
+        pass
+    else:
+        print('deleted {} files, see in {}'.format(
+            len(_fs.find_file_names(_deleted_dir)),
+            _fs.relpath(_deleted_dir, _fs.xpath('..'))
+        ), ':v8')
+    
+    _delete_file_a(fs_a.snapshot_file)
+    _delete_file_b(fs_b.snapshot_file)
+    fs_a.rebuild_snapshot(snap_new)
+    fs_b.rebuild_snapshot(snap_new)
+
+
+if __name__ == '__main__':
+    # pox projects/local_file_sync/main.py -h
+    
+    # pox projects/local_file_sync/main.py create_snapshot
+    #   ftp://172.20.128.123:2024/Likianta/documents/gitbook -r
+    # pox projects/local_file_sync/main.py create_snapshot
+    #   C:/Likianta/documents/gitbook -r
+    
+    # pox projects/local_file_sync/main.py sync_documents
+    #   C:/Likianta/documents/gitbook
+    #   ftp://172.20.128.123:2024/Likianta/documents/gitbook -d
+    cli.run()
